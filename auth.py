4029b891513c9d9c528e5ff71d09287773817ebc
IhorMudrytskyi <migor2k05@gmail.com>
2025-05-16 02:23:15 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -25,6 +25,10 @@ with tab[0]:
             st.header("Дані користувача: ")
             cursor.execute("select user_info.first_name, user_info.last_name, user_info.phone_number, user_info.email from user_info join auth on user_info.id = auth.id where auth.login = %s and auth.password = %s;",(auth_login, auth_password))
             info_result = cursor.fetchone()
+            st.text(f"Ім'я: {info_result[0]}")
+            st.text(f"Прізвище: {info_result[0]}")
+            st.text(f"Номер телефону: {info_result[0]}")
+            st.text(f"Електронна пошта: {info_result[0]}")
         else:
             st.warning("Неправильно введений логін чи пароль!")
 

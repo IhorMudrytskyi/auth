f142ba8f6e8e741899ea23e4f8476d446107733d
IhorMudrytskyi <migor2k05@gmail.com>
2025-05-19 19:32:05 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,71 +2,83 @@ import psycopg2
 import streamlit as st
 
 connection = psycopg2.connect(
-    host = st.secrets["psql"]["host"],
-    user = st.secrets["psql"]["user"],
-    port = st.secrets["psql"]["port"],
-    password = st.secrets["psql"]["password"],
-    database = st.secrets["psql"]["database"]
+    host = st.secrets["db"]["host"],
+    database = st.secrets["db"]["database"],
+    user = st.secrets["db"]["user"],
+    password = st.secrets["db"]["password"],
+    port = st.secrets["db"]["port"]
 )
-
 cursor = connection.cursor()
 connection.autocommit = True
 
-tab = st.tabs(["Авторизація", "Реєстрація", "Забув логін/пароль"])
+tab = st.tabs(["Автентицікая", "Реєстрація", "Відновлення паролю", "Зміна паролю"])
 
 with tab[0]:
+    st.header("Вхід")
     auth_login = st.text_input("Логін").replace(" ", "")
-    auth_password = st.text_input("Пароль", type='password').replace(" ", "")
+    auth_password = st.text_input("Пароль", type="password").replace(" ", "")
     if st.button("Увійти"):
-        cursor.execute("select login, password from auth where login = %s and password = %s;",(auth_login, auth_password))
-        auth_result = cursor.fetchone()
-        if auth_result:
-            st.success("Ви успішно авторизувалися!")
-            st.header("Дані користувача: ")
-            cursor.execute("select user_info.first_name, user_info.last_name, user_info.phone_number, user_info.email from user_info join auth on user_info.id = auth.id where auth.login = %s and auth.password = %s;",(auth_login, auth_password))
+        cursor.execute("select * from auth where login = %s and password = %s", (auth_login, auth_password))
+        result = cursor.fetchone()
+        if result:
+            st.success("Вхід успішний")
+            cursor.execute("select * from user_info join auth on user_info.id = auth.id where auth_login = %s and auth_password = %s", (auth_login,)
             info_result = cursor.fetchone()
-            first_name, last_name, phone_number, email = info_result
-            st.text(f"Ім'я: {first_name}")
-            st.text(f"Прізвище: {last_name}")
-            st.text(f"Номер телефону: {phone_number}")
-            st.text(f"Електронна пошта: {email}")
+            st.header("Дані користувача: ")
+            st.text(f"Ім'я: {info_result[1]}")
+            st.text(f"Прізвище: {info_result[2]}")
+            st.text(f"Номер телефону: {info_result[3]}")
+            st.text(f"Електронна пошта: {info_result[4]}")
+
         else:
-            st.warning("Неправильно введений логін чи пароль!")
+            st.error("Неправильний логін або пароль!")
 
 with tab[1]:
+    st.header("Реєстрація")
+    reg_login = st.text_input("Логін ").replace(" ", "")
+    reg_password = st.text_input("Пароль ", type="password").replace(" ", "")
     reg_first_name = st.text_input("Ім'я ").replace(" ", "")
     reg_last_name = st.text_input("Прізвище ").replace(" ", "")
     reg_phone_number = st.text_input("Номер телефону ").replace(" ", "")
     reg_email = st.text_input("Електронна пошта ").replace(" ", "")
-    reg_login = st.text_input("Логін ").replace(" ", "")
-    reg_password = st.text_input("Пароль ").replace(" ", "")
-    if st.button("Створити акаунт"):
-        cursor.execute("select login from auth where login = %s;",(reg_login,))
+    if st.button("Зареєструватися"):
+        cursor.execute("select login from auth where login = %s", (reg_login,))
         login_result = cursor.fetchone()
-        if login_result:
-            st.warning("Користувач з таким логіном вже існує!")
+        if result:
+            st.error("Користувач з таким логіном вже існує!")
         else:
-            cursor.execute("select email from user_info where email = %s;",(reg_email,))
+            cursor.execute("select email from user_info where email = %s", (reg_email,))
             email_result = cursor.fetchone()
             if email_result:
-                st.warning("Користувач з такою електронною поштою вже існує!")
-            else:
-                cursor.execute("insert into auth(login, password) values (%s, %s) returning id;",(reg_login, reg_password))
+                cursor.execute("insert into auth(login, password) values (%s, %s) returning id;", (reg_login, reg_password))
                 reg_id = cursor.fetchone()
-                cursor.executex("insert into user_info(id, first_name, last_name, phone_number, email) values (%s, %s, %s, %s, %s);",(reg_id, reg_first_name, reg_last_name, reg_phone_number, reg_email))
-                st.succes("Акаунт успішно створено!")
+                cursor.execute("insert into user_info(id, first_name, last_name, phone_number, email) values (%s, %s, %s, %s, %s);", (reg_id, reg_first_name, reg_last_name, reg_phone_number, reg_email))
+                st.success("Реєстрація успішна!")
 
 with tab[2]:
-    zab_first_name = st.text_input("Ім'я  ").replace(" ", "")
-    zab_last_name = st.text_input("Прізвище  ").replace(" ", "")
-    zab_phone_number = st.text_input("Номер телефону  ").replace(" ", "")
-    zab_email = st.text_input("Електронна пошта  ").replace(" ", "")
-    if st.button("Нагадати"):
-        cursor.execute("select auth.login, auth.password from auth join user_info in auth.id = user_info.id where user_info.first_name = %s and user_info.last_name = %s and user_info.phone_number = %s and user_info.email = %s;",(zab_first_name, zab_last_name, zab_phone_number, zab_email))
-        zab_result = cursor.fetchone()
-        if zab_result:
-            st.header("Дані для авторизації: ")
-            st.text(f"Логін: {zab_result[0]}")
-            st.text(f"Пароль: {zab_result[1]}")
+    st.header("Відновлення паролю")
+    reset_login = st.text_input("Логін   ").replace(" ", "")
+    reset_email = st.text_input("Електронна пошта   ").replace(" ", "")
+    if st.button("Відновити пароль"):
+        cursor.execute("select * from auth where login = %s and email = %s", (reset_login, reset_email))
+        result = cursor.fetchone()
+        if result:
+            new_password = st.text_input("Новий пароль", type="password").replace(" ", "")
+            cursor.execute("update auth set password = %s where login = %s", (new_password, reset_login))
+            st.success("Пароль успішно змінено!")
         else:
-            st.warning("Користувача з такими даними не знайдено!")
+            st.error("Користувача з таким логіном та електронною поштою не існує!")
+
+with tab[3]:
+    st.header("Зміна паролю")
+    change_login = st.text_input("Логін    ").replace(" ", "")
+    change_email = st.text_input("Електронна пошта    ").replace(" ", "")
+    if st.button("Змінити пароль"):
+        cursor.execute("select * from auth where login = %s and email = %s", (change_login, change_email))
+        result = cursor.fetchone()
+        if result:
+            new_password = st.text_input("Новий пароль", type="password").replace(" ", "")
+            cursor.execute("update auth set password = %s where login = %s", (new_password, change_login))
+            st.success("Пароль успішно змінено!")
+        else:
+            st.error("Користувача з таким логіном та електронною поштою не існує!")
